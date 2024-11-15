86a286c806b5bb6f18e6f3e7f0f06297eeb79e3b
BuHT <thisisbuht@gmail.com>
2024-11-15 19:36:04 +0300

    Fixed project controller and added lombok

--- springdemo/src/main/java/ru/roh/springdemo/controllers/ProjectController.java
+++ springdemo/src/main/java/ru/roh/springdemo/controllers/ProjectController.java
@@ -1,7 +1,8 @@
 package ru.roh.springdemo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.roh.springdemo.models.Project;
 import ru.roh.springdemo.services.ProjectService;
@@ -11,38 +12,55 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
+@RequiredArgsConstructor
 public class ProjectController {
 
-    @Autowired
-    private ProjectService projectService;
+    private final ProjectService projectService;
 
-    // Создание нового проекта
+    /**
+     * Creates project.
+     *
+     * @param project project dto to create
+     * @return ResponseEntity with created project
+     */
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Project createProject(@RequestBody Project project) {
-        return projectService.createProject(project);
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(projectService.createProject(project));
     }
 
-    // Получение проекта по ID
+    /**
+     * Get project by id.
+     *
+     * @param id project id
+     * @return project
+     */
     @GetMapping("/{id}")
-    public Project getProjectById(@PathVariable Long id) {
+    public Project getProject(@PathVariable Long id) {
         return projectService.getProjectById(id);
     }
 
-    // Получение всех проектов
+    /**
+     * Get all projects.
+     *
+     * @return list of all projects
+     */
     @GetMapping
     public List<Project> getAllProjects() {
         return projectService.getAllProjects();
     }
 
-    // Поиск проектов по названию
-    @GetMapping("/search")
+    /**
+     * Finds projects by name.
+     *
+     * @return list of projects
+     */
+    @GetMapping("/findProjectsByName")
     public List<Project> searchProjects(@RequestParam String name) {
         return projectService.getProjectsByName(name);
     }
 
     // Поиск проектов, начинающихся после указанной даты
-    @GetMapping("/startDate")
+    @GetMapping("/findProjectsByDate")
     public List<Project> searchProjectsByStartDate(@RequestParam LocalDateTime startDate) {
         return projectService.getProjectsByStartDate(startDate);
     }
